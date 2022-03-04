8b72425effb92e95bd9a94d596128066396ca4d4
jianfeng <gjf610@yeah.net>
2022-03-04 18:23:10 +0800

    type: added error type

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import * as https from 'https';
-import * as md5 from 'md5';
 import * as querystring from 'querystring'
+import md5 from 'md5';
 import { appId, appSecret } from './private'
 
-const errorMap = {
+type ErrorMap = {
+  [k: string]: string
+}
+const errorMap: ErrorMap = {
   52001: '请求超时',
   52002: '系统错误',
   52003: '用户认证失败',
@@ -13,11 +16,10 @@ const errorMap = {
   54004: '账户余额不足',
   54005: '长query请求频繁',
   90107: '认证未通过或未生效',
-  unknown: '服务器繁忙',
 }
 
 
-export const translate = (words) => {
+export const translate = (words: string) => {
 
   const salt = Math.random()
   const sign = md5(appId + words + salt + appSecret)
@@ -43,8 +45,8 @@ export const translate = (words) => {
     method: 'GET'
   };
   const request = https.request(options, (response) => {
-    let chunks = []
-    response.on('data', (chunk) => {
+    let chunks: Buffer[] = []
+    response.on('data', (chunk: Buffer) => {
       chunks.push(chunk)
     });
     response.on('end', () => {
