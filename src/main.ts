1631bd060af9b7c672219ccf642c531d224d00aa
jianfeng <gjf610@yeah.net>
2022-03-04 18:01:13 +0800

    feat: added errorMap

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import * as querystring from 'querystring'
 import { appId, appSecret } from './private'
 
 const errorMap = {
+  52001: '请求超时',
+  52002: '系统错误',
   52003: '用户认证失败',
-  52004: 'error2',
-  52005: '',
+  54000: '必填参数为空',
+  54001: '签名错误',
+  54003: '访问频率受限',
+  54004: '账户余额不足',
+  54005: '长query请求频繁',
+  90107: '认证未通过或未生效',
   unknown: '服务器繁忙',
 }
 
